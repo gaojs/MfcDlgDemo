73522f5feac1f971d09d7a47003a7ed582b7fb7e
hill.gao <hill.gao@yuansuan.com>
2023-11-29 14:09:51 +0800

    创建个默认的对话框，点击按钮，模拟应用程序无响应

---
+++ MfcDlgDemo/MfcDlgDemo.h
@@ -0,0 +1,32 @@
+﻿
+// MfcDlgDemo.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMfcDlgDemoApp:
+// 有关此类的实现，请参阅 MfcDlgDemo.cpp
+//
+
+class CMfcDlgDemoApp : public CWinApp
+{
+public:
+	CMfcDlgDemoApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMfcDlgDemoApp theApp;
---
+++ MfcDlgDemo/MfcDlgDemoDlg.h
@@ -0,0 +1,36 @@
+﻿
+// MfcDlgDemoDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMfcDlgDemoDlg 对话框
+class CMfcDlgDemoDlg : public CDialogEx
+{
+// 构造
+public:
+	CMfcDlgDemoDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCDLGDEMO_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+      public:
+        afx_msg void OnBnClickedOk();
+};
